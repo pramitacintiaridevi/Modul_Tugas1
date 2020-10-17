b4b0b14748ab0adbb4f79d39457622697a24c90f
pramitacintiaridevi <72422138+pramitacintiaridevi@users.noreply.github.com>
2020-10-17 14:50:41 +0700

    Modul dan Tugas Modul 1 fundamental OOP

---
+++ src/com/Cintiari/Modul1/Kelas.java
@@ -0,0 +1,16 @@
+package com.Cintiari.Modul1;
+
+public class Kelas {
+    public static String nama = "Pramita";
+
+    private static int Setminute(){
+        int minute = 10;
+        return minute;
+    }
+
+    public static void main(String[] args) {
+       System.out.println("nama\t\t:" + nama);
+       System.out.println("Set Minute\t:" + Setminute());
+
+    }
+}
